ab1c7fefd0e6d93727e74326b2a4f74c9f032e81
evepy <ejuana.bsrz@gmail.com>
2024-10-13 12:31:00 -0300

    primer contrato

---
+++ hola_mundo.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.25;
+
+//logica del contrato inteligente
+contract hola_mundo{
+    //declarar variables y su contenido
+    string public saludo = "Hola Mundo";
+
+}
